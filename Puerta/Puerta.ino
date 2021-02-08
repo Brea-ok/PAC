d5d4716a9c16fd23cf62d2ddd56955973b46e2dc
Rocio Martinez <rocio.martinez.civetta@gmail.com>
2021-02-08 17:20:19 -0300

    Actualizacion Puerta

--- Puerta/Puerta.ino
+++ Puerta/Puerta.ino
@@ -35,85 +35,20 @@ void setup()
 
 void loop()
 {
- 
     digitalWrite(trigPin, LOW);  // Added this line
     delayMicroseconds(2); // Added this line
     digitalWrite(trigPin, HIGH);
-   delayMicroseconds(10); // Added this line
+    delayMicroseconds(10); // Added this line
     digitalWrite(trigPin, LOW);
     duration1 = pulseIn(echoPin, HIGH);
     distance1 = (duration1/2) / 29.1;
-  
-  if (distance1<=40)
-
-  {
-    myDFPlayer.play(1);  //Play next mp3 every 3 second.
-    delay(1000);
+    Serial.println(distance1);
+  if (distance1<=40){
+    myDFPlayer.play(1);
+    Serial.println("sono");
+    delay(5000);
   }
-  
-  if (myDFPlayer.available()) {
-    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
-  }
+  delay(200);
 }
 
-void printDetail(uint8_t type, int value){
-  switch (type) {
-    case TimeOut:
-      Serial.println(F("Time Out!"));
-      break;
-    case WrongStack:
-      Serial.println(F("Stack Wrong!"));
-      break;
-    case DFPlayerCardInserted:
-      Serial.println(F("Card Inserted!"));
-      break;
-    case DFPlayerCardRemoved:
-      Serial.println(F("Card Removed!"));
-      break;
-    case DFPlayerCardOnline:
-      Serial.println(F("Card Online!"));
-      break;
-    case DFPlayerUSBInserted:
-      Serial.println("USB Inserted!");
-      break;
-    case DFPlayerUSBRemoved:
-      Serial.println("USB Removed!");
-      break;
-    case DFPlayerPlayFinished:
-      Serial.print(F("Number:"));
-      Serial.print(value);
-      Serial.println(F(" Play Finished!"));
-      break;
-    case DFPlayerError:
-      Serial.print(F("DFPlayerError:"));
-      switch (value) {
-        case Busy:
-          Serial.println(F("Card not found"));
-          break;
-        case Sleeping:
-          Serial.println(F("Sleeping"));
-          break;
-        case SerialWrongStack:
-          Serial.println(F("Get Wrong Stack"));
-          break;
-        case CheckSumNotMatch:
-          Serial.println(F("Check Sum Not Match"));
-          break;
-        case FileIndexOut:
-          Serial.println(F("File Index Out of Bound"));
-          break;
-        case FileMismatch:
-          Serial.println(F("Cannot Find File"));
-          break;
-        case Advertise:
-          Serial.println(F("In Advertise"));
-          break;
-        default:
-          break;
-      }
-      break;
-    default:
-      break;
-  }
   
-}
